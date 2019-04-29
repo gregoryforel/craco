a770db7352d7eef0aece9b500a14c28a2f13347b
gregoryforel <gregory.forel@gmail.com>
2019-04-29 23:23:41 +0200

    works with normal LESS

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,70 @@
 const CracoAntDesignPlugin = require("craco-antd");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const WebpackBar = require("webpackbar");
+
+// Don't open the browser during development
+process.env.BROWSER = "none";
 
 module.exports = {
+  webpack: {
+    plugins: [
+      new WebpackBar({ profile: true }),
+      ...(process.env.NODE_ENV === "development"
+        ? [new BundleAnalyzerPlugin({ openAnalyzer: false })]
+        : []),
+    ],
+  },
   plugins: [
     {
       plugin: CracoAntDesignPlugin,
 
-      // theme can be customize here, or using `antd.customize.less`
-      // options: {
-      //   customizeTheme: {
-      //     "@primary-color": "#1DA57A",
-      //     "@link-color": "#1DA57A"
-      //   }
-      // }
+      options: {
+        modifyLessRule: function(lessRule, _context) {
+          // lessRule.test = /\.(module)\.(less)$/;
+          // lessRule.test = /\.less$/;
+          lessRule.test = /\.less$/;
+          // lessRule.exclude = /node_modules/;
+          return lessRule;
+        },
+        // modifyLessRule: function(lessRule, _context) {
+        // 	// lessRule.test = /\.(module)\.(less)$/;
+        // 	// lessRule.test = /\.less$/;
+        // 	lessRule.test = /\.less$/
+        // 	// lessRule.exclude = /node_modules/;
+        // 	lessRule.use = [
+        // 		{
+        // 			loader: 'typings-for-css-modules-loader', // generates typings for CSS and LESS files on the fly.
+        // 			query: {
+        // 				modules: true,
+        // 				importLoaders: 1,
+        // 				camelCase: true,
+        // 				namedExport: true,
+        // 				localIdentName: '[name]_[local]_[hash:base64:5]'
+        // 			}
+        // 		},
+        // 		{
+        // 			loader: 'postcss-loader',
+        // 			options: { sourceMap: true }
+        // 		},
+        // 		{
+        // 			loader: 'less-loader',
+        // 			options: {
+        // 				javascriptEnabled: true
+        // 			}
+        // 		}
+        // 	]
+        // 	return lessRule
+        // },
+
+        // cssLoaderOptions: {
+        //   modules: true,
+        //   localIdentName: `${
+        //     process.env.NODE_ENV === "production"
+        //       ? "[local]_[hash:base64:5]"
+        //       : "[path][name]__[local]-"
+        //   }-[hash:base64:8]`,
+        // },
+      },
     },
   ],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import _ from "lodash";
-import React, { Component } from "react";
-import "./App.css";
-import Hello from "./Hello";
-import logo from "./logo.svg";
+import { Button } from "antd";
+import * as React from "react";
 
-class App extends Component {
+import * as styles from "./App.less";
+
+class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {_.map(_.range(5), (it: number) => (
-            <div key={it}>{it}</div>
-          ))}
-          <Hello
-            name={"Nelly Sachs"}
-            onClickButton={({ name }) => {
-              return new Promise(res => {
-                setTimeout(res, 1000);
-              });
-            }}
-          />
-        </header>
+      <div className={styles.app}>
+        hi
+        <Button type="danger">Antd button</Button>
       </div>
     );
   }
